fa220b04e794d7ee23950822312cada82abaa94c
Jose Pereira <jose.pereira@wizeline.com>
2020-12-01 13:14:58 -0500

    feat():Initial project and basic data manipulation

---
+++ newHero/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <h1>Heroes</h1>
+  <ul>
+    <li v-for="hero in heroes" :key="hero.uuid">
+      {{ hero.uuid }}:{{ hero.name }}
+    </li>
+  </ul>
+  <form @submit.prevent="addHero">
+    <input v-model="newHero" placeholder="Add Hero" />
+    <button type="submit">Add Hero</button>
+  </form>
+</template>
+
+<script>
+import _ from "underscore";
+export default {
+  data() {
+    return {
+      newHero: "",
+      heroes: [
+        { uuid: _.uniqueId(), name: "Batman" },
+        { uuid: _.uniqueId(), name: "Superman" },
+        { uuid: _.uniqueId(), name: "Wonder Woman" },
+      ],
+    };
+  },
+  methods: {
+    addHero() {
+      if (this.newHero !== "") {
+        this.heroes.push({ uuid: _.uniqueId(), name: this.newHero });
+        this.newHero = "";
+      }
+    },
+  },
+};
+</script>
