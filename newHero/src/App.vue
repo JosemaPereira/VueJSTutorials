d8dd7101fa71c039095c163fee9bd9f7852beb96
JosemaPereira <josema.pereira.cih@gmail.com>
2020-12-02 11:02:56 -0500

    feat(): add computer funtions

--- newHero/src/App.vue
+++ newHero/src/App.vue
@@ -1,14 +1,19 @@
 <template>
   <div>
-    <h1>Heroes</h1>
+    <h1>Heroes {{ herosCount }}</h1>
+    <h2>{{ fullName }}</h2>
     <ul>
       <li v-for="hero in heroes" :key="hero.uuid">
-        {{ hero.uuid }}:{{ hero.name }}
+        <p>
+          {{ hero.uuid }}:{{ hero.name }}
+          <button @click="remove(hero.uuid)">X</button>
+        </p>
       </li>
     </ul>
     <form @submit.prevent="addHero">
       <input v-model="newHero" placeholder="Add Hero" />
       <button type="submit">Add Hero</button>
+      <button @click="setFullName">Set fullName</button>
     </form>
   </div>
 </template>
@@ -19,6 +24,8 @@ export default {
   data() {
     return {
       newHero: "",
+      fName: "Josema",
+      lName: "Pereira",
       heroes: [
         { uuid: _.uniqueId(), name: "Batman" },
         { uuid: _.uniqueId(), name: "Superman" },
@@ -32,6 +39,27 @@ export default {
         this.heroes.push({ uuid: _.uniqueId(), name: this.newHero });
         this.newHero = "";
       }
+    },
+    setFullName() {
+      this.fullName = "Tamara Sabido";
+    },
+    remove(uuid) {
+      this.heroes = this.heroes.filter((hero) => hero.uuid !== uuid);
+    },
+  },
+  computed: {
+    herosCount() {
+      return this.heroes.length;
+    },
+    fullName: {
+      get() {
+        return `${this.fName} ${this.lName}`;
+      },
+      set(fullName) {
+        const values = fullName.split(" ");
+        this.fName = values[0];
+        this.lName = values[1];
+      },
     },
   },
 };
