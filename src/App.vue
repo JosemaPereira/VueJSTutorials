0951c41050aa7ce1409870fed746b5372a5f4afb
JosemaPereira <josema.pereira.cih@gmail.com>
2020-12-02 13:20:29 -0500

    feat(): add calendar component and basic render

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <Header />
   <div class="w-full flex">
-    <Heroes />
+    <!-- <Heroes /> -->
+    <Calendar />
   </div>
 </template>
 
 <script>
 import Header from "./components/Header";
-import Heroes from "./components/Heroes";
+// import Heroes from "./components/Heroes";
+import Calendar from "./components/Calendar";
 
 export default {
-  components: { Header, Heroes },
+  components: { Header, Calendar },
 };
 </script>
