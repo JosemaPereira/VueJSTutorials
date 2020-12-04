1b4fd63bf1fcb6d78068a3e961a95fed75320267
JosemaPereira <josema.pereira.cih@gmail.com>
2020-12-04 11:01:44 -0500

    feat(): Add modal interactions

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,9 +2,10 @@
   <nav
     class="w-full bg-gradient-to-r from-blue-800 to-blue-600 text-white px-4 py-2"
   >
-    <router-link class="mx-2" v-for="item in list" :to="item.to" :key="item.to"
-      ><a>{{ item.title }}</a></router-link
-    >
+    <router-link class="mx-2" v-for="item in list" :to="item.to" :key="item.to">
+      <a>{{ item.title }}</a>
+    </router-link>
+    <button class="mx-2" @click="$emit('open-login-modal')">Login</button>
   </nav>
 </template>
 
